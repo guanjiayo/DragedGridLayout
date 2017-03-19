33287375d399619c832cd2ca43ddc1365b9f67dd
moxiaoming <mrmonkeyq@163.com>
2017-03-19 10:59:04 +0800

    222

--- MyApplication/gridlayout/src/main/java/zs/xmx/wygridlayout/BBActivity.java
+++ MyApplication/gridlayout/src/main/java/zs/xmx/wygridlayout/BBActivity.java
@@ -3,11 +3,11 @@ package zs.xmx.wygridlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DDActivity extends AppCompatActivity {
+public class BBActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dd);
+        setContentView(R.layout.activity_bb);
     }
 }
